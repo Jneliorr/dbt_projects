1a32f7fd61ff7516523b1ea937b178a4ee968730
Jneliorr <jneliorr@gmail.com>
2025-05-08 10:20:59 -0300

    decred e cnpj corrigidios

--- decred/models/staging/decred_raw.sql
+++ decred/models/staging/decred_raw.sql
@@ -7,5 +7,12 @@
 
     
 SELECT
-*
-FROM `infra-itaborai.decred.decred`
+*,
+  CASE
+  WHEN DS_OPERACAO_TIPO_NATUREZA = 'Crédito' THEN 'Crédito'
+  WHEN DS_OPERACAO_TIPO_NATUREZA = 'CrÃ©dito' THEN 'Crédito'
+  WHEN DS_OPERACAO_TIPO_NATUREZA = 'Débito' THEN 'Débito'
+  WHEN DS_OPERACAO_TIPO_NATUREZA = 'DÃ©bito' THEN 'Débito'
+  end as DS_OPERACAO_TIPO_NATUREZA_OK
+FROM `infra-itaborai.mds_decred.decred`
+
